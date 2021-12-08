8c4dd0cdfbed06108ab249fd0d1e4e918c8441ce
unknown <k2142a@gmail.com>
2021-12-08 20:35:42 +0700

    add preview

--- js/slide-spec.js
+++ js/slide-spec.js
@@ -1,7 +1,9 @@
 const specBtn = document.getElementById("spec-open");
 const specBtnClose = document.getElementById("spec-close");
 const specArea = document.getElementById("spec-area");
-let checkSpec = true;
+const previewDown = document.getElementById("preview-open");
+const previewUp = document.getElementById("preview-close");
+const previewArea = document.getElementById("preview-area");
 
 specBtn.addEventListener("click", function () {
   specArea.classList.toggle("reveal-area");
@@ -13,4 +15,16 @@ specBtnClose.addEventListener("click", function () {
   specArea.classList.toggle("reveal-area");
   specBtnClose.classList.toggle("spec-hidden");
   specBtn.classList.toggle("spec-hidden");
+});
+
+previewDown.addEventListener("click", function () {
+  previewArea.classList.toggle("preview-slide");
+  previewDown.classList.toggle("preview-hidden");
+  previewUp.classList.toggle("preview-hidden");
+});
+
+previewUp.addEventListener("click", function () {
+  previewArea.classList.toggle("preview-slide");
+  previewDown.classList.toggle("preview-hidden");
+  previewUp.classList.toggle("preview-hidden");
 });
